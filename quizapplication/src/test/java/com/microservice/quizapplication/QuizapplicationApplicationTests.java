3e974fd14e4b94c739de2247a752c271f7a18d2c
sagar h <sagarjamadar1995@gmail.com>
2024-07-20 13:55:15 +0530

    added new project

---
+++ quizapplication/src/test/java/com/microservice/quizapplication/QuizapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.quizapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
